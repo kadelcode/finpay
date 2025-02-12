acccf0a79aedd75b042768dc97357ed5528b9b56
kadelcode <kadelcode@gmail.com>
2025-02-12 17:34:24 +0100

    Add user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    phone_number = models.CharField(max_length=15, unique=True)
+    is_verified = models.BooleanField(default=False)
+    date_of_birth = models.DateField(null=True, blank=True)
